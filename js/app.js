9c6aa056d1baf3e93c2e86a03dac2c5408a90b83
Christina Hollie <christinahollie7@gmail.com>
2023-02-19 19:17:13 -0800

    added more css

--- js/app.js
+++ js/app.js
@@ -1,49 +1,168 @@
 'use strict';
 
-let intro = prompt('What is your name?');
-alert('Welcome ' + intro + '! Let\'s begin the quiz!✨ (yes/no or y/n answers)');
+// 
+
+function getName(userName){
+  return(userName);
+}
+
+let userName = prompt('What is your name?');
+if (userName) {
+  console.log(getName);
+  alert(`Welcome ${userName}! Let's begin the quiz!✨ (yes/no or y/n answers)`);
+
+}
 
 
-let orgin = prompt('Was I born in San Fransisco?');
+// // let intro = prompt('What is your name?');
+// // alert('Welcome ' + intro + '! Let\'s begin the quiz!✨ (yes/no or y/n answers)');
 
-if (orgin === 'no' || orgin === 'n') {
+function questionOne(){
+  let userInput = prompt('Was I born in San Fransisco?');
+  if (userInput === 'no' || userInput === 'n'){
     alert('You got it right! I was born in Walnut Creek');
-} else if (orgin === 'yes' || orgin === 'y'); {
+    console.log('You got it right! I was born in Walnut Creek');
+  } else if (userInput === 'yes' || userInput ==='y'){
     alert('Maybe next time.🤷🏽‍♀️ I was actually born in Walnut Creek.');
+  }
 }
+questionOne();
 
+// // let orgin = prompt('Was I born in San Fransisco?');
 
-let favortiteColor = prompt('Do you think my favorite color is green?');
+// // if (orgin === 'no' || orgin === 'n') {
+// //   alert('You got it right! I was born in Walnut Creek');
+// // } else if (orgin === 'yes' || orgin === 'y'); {
+// //   alert('Maybe next time.🤷🏽‍♀️ I was actually born in Walnut Creek.');
+// // }
 
-if (favortiteColor === 'yes' || favortiteColor ==='y') {
-    //console.log('wrong')
-    alert('Sorry, try again.');
-} else if (favortiteColor === 'no' || favortiteColor === 'n') {
-    alert('Correct! It\'s actually purple!💜')
+function questionTwo(){
+  let userInput = prompt('Do you think my favorite color is green?');
+  if (userInput === 'no' || userInput === 'n'){
+    alert('Correct! It\'s actually purple!💜');
+    console.log('Correct! It\'s actually purple!💜');
+  } else if (userInput === 'yes' || userInput ==='y'){
+    alert('wrong');
+    console.log('wrong');
+  }
 }
+questionTwo();
 
-let team = prompt('Is my favorite basketball team the Lakers?');
+// // let favortiteColor = prompt('Do you think my favorite color is green?');
 
-if (team === 'yes' || team === 'y') {
-    alert('It sure is! Don\'t be a hater!💛💜');
-} else if (team === 'no' || team === 'n') {
+// // if (favortiteColor === 'yes' || favortiteColor ==='y') {
+// //   //console.log('wrong')
+// //   alert('Sorry, try again.');
+// // } else if (favortiteColor === 'no' || favortiteColor === 'n') {
+// //   alert('Correct! It\'s actually purple!💜');
+// // }
+
+function questionThree(){
+  let userInput = prompt('Is my favorite basketball team the Lakers?');
+  if (userInput === 'no' || userInput === 'n'){
     alert('YOU\'RE WRONG!!!🤷🏽‍♀️');
+    console.log('YOU\'RE WRONG!!!🤷🏽‍♀️');
+  } else if (userInput === 'yes' || userInput ==='y'){
+    alert('It sure is! Don\'t be a hater!💛💜');
+    console.log('It sure is! Don\'t be a hater!💛💜');
+  }
 }
+questionThree();
 
-let dreamCity = prompt('Do you think my dream destination is Thailand?');
+// let team = prompt('Is my favorite basketball team the Lakers?');
 
-if (dreamCity === 'yes' || dreamCity === 'y') {
-    alert('Sorry, wrong continent. My dream destination is Italy🍝🍷');
-} else if (dreamCity === 'no' || dreamCity === 'n') {
+// if (team === 'yes' || team === 'y') {
+//   alert('It sure is! Don\'t be a hater!💛💜');
+// } else if (team === 'no' || team === 'n') {
+//   alert('YOU\'RE WRONG!!!🤷🏽‍♀️');
+// }
+
+function questionFour(){
+  let userInput = prompt('Do you think my dream destination is Thailand?');
+  if (userInput === 'no' || userInput === 'n'){
     alert('You are correct! Italy is my dream.🍝🍷');
+    console.log('You are correct! Italy is my dream.🍝🍷');
+  } else if (userInput === 'yes' || userInput ==='y'){
+    alert('Sorry, wrong continent. My dream destination is Italy🍝🍷');
+    console.log('Sorry, wrong continent. My dream destination is Italy🍝🍷');
+  }
+}
+questionFour();
+
+// let dreamCity = prompt('Do you think my dream destination is Thailand?');
+
+// if (dreamCity === 'yes' || dreamCity === 'y') {
+//   alert('Sorry, wrong continent. My dream destination is Italy🍝🍷');
+// } else if (dreamCity === 'no' || dreamCity === 'n') {
+//   alert('You are correct! Italy is my dream.🍝🍷');
+// }
+
+function questionFive(){
+  let userInput = prompt('Is my favorite cuisine Mexican?');
+  if (userInput === 'no' || userInput === 'n'){
+    alert('So close. Try again.😖');
+    console.log('So close. Try again.😖');
+  } else if (userInput === 'yes' || userInput ==='y'){
+    alert('You did it!!🎊');
+    console.log('You did it!!🎊');
+  }
+}
+questionFive();
+
+// let food = prompt('Is my favorite cuisine Mexican?');
+
+// if (food === 'yes' || food === 'y') {
+//   alert('You did it!!🎊');
+// } else if (food === 'no' || food === 'n'); {
+//   alert('So close. Try again.😖');
+// }
+
+function numberGuess(guessNumber) {
+  let guess = prompt (guessNumber);
+  let numberAttempts = 4;
+  while(numberAttempts){
+    if (guess > 7){
+      alert ('Sorry, your guess is too high');
+      numberAttempts--;
+    } else if(guess < 7){
+      alert ('So close, but not quite');
+      numberAttempts--;
+    } else if (guess === 7){
+      alert ('Yes! You did it! 🔮');
+      numberAttempts = 0;
+      break;
+    }
+  }
+  if (!numberAttempts){
+    alert('You are all out of tries.🤷🏽‍♀️ Thanks for playing!');
+  }
 }
 
-let food = prompt('Is my favorite cuisine Mexican?');
+numberGuess('Can you guess my lucky number? You must do this within 4 attempts.');
 
-if (food === 'yes' || food === 'y') {
-    alert('You did it!!🎊'); 
-}  else if (food === 'no' || food === 'n'); {
-    alert('So close. Try again.😖');
+
+function numberGuess() {
+    let numberGuess = 7;
+    //   let score = 0;
+    let numberAttempts = 4;
+
+    while (numberAttempts) {
+        let guess = prompt(`Can you guess my lucky number? You must do this within ${numberAttempts} attempts.`);
+        numberAttempts--;
+
+        if (numberGuess > 7) {
+            alert('Sorry, your guess is too high');
+        } else if (guess < 7) {
+            alert('So close, but not quite');
+        } else if (numberGuess === 7) {
+            alert('Yes! You did it! 🔮');
+            break;
+            //     score++;
+        }
+        if (!numberAttempts) {
+            alert('You are all out of tries.🤷🏽‍♀️ Thanks for playing!');
+        }
+    }
 }
 
 let numberGuess = 7;
@@ -58,7 +177,7 @@ if (guess > 7){
     alert ('Sorry, your guess is too high');
 } else if(guess < 7){
     alert ('So close, but not quite');
-} else if (guess === 7){
+} else if (guess = 7){
     alert ('Yes! You did it!=🔮');
 break;
 score++
@@ -75,7 +194,7 @@ let choice = prompt('What is one of my favorite movie?')
 
 for (let i = 0; i < favMovies.length; i++) {
 console.log(favMovies[i])
-if (choice === favMovies[i]){
+if (choice = favMovies[i]){
     alert(`You have chosen wisely! ${choice} is one of my favorite movies!`);
 } 
 }
